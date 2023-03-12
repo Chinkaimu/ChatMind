e9f5f7ff5e739b87c7a104bdbbcdfb0a4438eab3
Qing Wang ⚛ <devrsi0n@gmail.com>
2023-03-12 17:51:16 +0800

    feat: add logo

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import clsx from "clsx";
 import { SignedIn, UserButton, SignedOut, SignInButton } from "@clerk/nextjs";
+import { Logo } from "./logo";
 
 export type HeaderProps = {
   // children: React.ReactNode;
@@ -8,16 +9,19 @@ export type HeaderProps = {
 
 export function Header(props: HeaderProps): JSX.Element {
   return (
-    <header className="fixed top-0 w-full bg-white/75 py-4 backdrop-blur-md z-10">
-      <div className="mx-auto flex max-w-3xl items-center justify-end">
-        <SignedIn>
-          {/* Mount the UserButton component */}
-          <UserButton />
-        </SignedIn>
-        {/* Signed out users get sign in button */}
-        <SignedOut>
-          <SignInButton />
-        </SignedOut>
+    <header className="fixed top-0 z-10 w-full border-b bg-white/75 py-4 backdrop-blur-md">
+      <div className="mx-auto flex max-w-3xl items-center justify-between">
+        <Logo />
+        <div>
+          <SignedIn>
+            {/* Mount the UserButton component */}
+            <UserButton />
+          </SignedIn>
+          {/* Signed out users get sign in button */}
+          <SignedOut>
+            <SignInButton />
+          </SignedOut>
+        </div>
       </div>
     </header>
   );
