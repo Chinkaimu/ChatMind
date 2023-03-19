cd5cde2cccdbac923f64c4ce075c79e8ec8982fd
Qing <7880675+devrsi0n@users.noreply.github.com>
2023-03-19 21:05:24 +0800

    feat: add tinybird integration

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { Inter } from "next/font/google";
 import "~/styles/globals.css";
 import { Toaster } from "../components";
 import Head from "next/head";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -104,6 +105,12 @@ const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
         <Component {...pageProps} />
       </ClerkProvider>
       <Toaster />
+      <Script
+        strategy="lazyOnload"
+        src="https://unpkg.com/@tinybirdco/flock.js"
+        data-host="https://api.tinybird.co"
+        data-token="p.eyJ1IjogIjA0NDI3NDEzLTI1Y2ItNGU3Mi05YTgzLThhNGRkZWZkMjIxMCIsICJpZCI6ICI2ZGY3MWE0MC0yNDRhLTQyZjctYmY5MC1kY2NiNjhlNGMxNGIifQ.j3JfEDNF01Xt5057jRZJVZv1RsLKKwkqst7KwVks7Kg"
+      />
     </>
   );
 };
