8c19f4ab0b681bb1a51f870d9cc6f64cc317cbcc
Qing Wang ⚛ <devrsi0n@gmail.com>
2023-03-21 20:34:44 +0800

    feat: use new logo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -32,37 +32,14 @@ const MyApp: AppType = ({ Component, pageProps }: AppProps) => {
           name="description"
           content="Enhancing your ChatGPT experience with ChatMind."
         />
-        <link rel="icon" href="/favicon.svg" />
-
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
-
+        <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link
           rel="icon"
           type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
+          sizes="196x196"
+          href="/assets/icons/favicon-196.png"
         />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
+        <link rel="apple-touch-icon" href="/assets/icons/apple-touch-icon.png"/>
         <link rel="manifest" href="/manifest.json" />
         <link
           rel="mask-icon"
